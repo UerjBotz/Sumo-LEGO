752446eec8f2d7ef04188fbc67f2272d29f7928f
Ian <ian@phygitall.rio>
2024-06-20 19:32:40 -0300

    consertando erros

--- SUMOLEGO/test.py
+++ SUMOLEGO/test.py
@@ -16,8 +16,8 @@ import random
 from time import time
 
 EDGE_THRESHOLD = 5
-ENEMY_DISTANCE = 100
-SPEED_RUN = 100  # Power %
+ENEMY_DISTANCE = 150
+SPEED_RUN = 100  # Power 
 SPEED_SEARCH = 360  # Deg/s
 
 ev3 = EV3Brick()
@@ -28,124 +28,101 @@ def countdown():
         wait(900)
         ev3.speaker.beep(frequency=1000, duration=100)
 
-
-def lower_wedge():
-    # Lower the wedge while the other stuff is initialising
-    motor_W.run_until_stalled(speed=500, then=Stop.HOLD, duty_limit=50)
-    # Lift the wedge just a bit to avoid dragging it on the floor and losing traction
-    motor_W.run_angle(speed=500, rotation_angle=-10, then=Stop.HOLD, wait=False)
-
-# Acionará a tração para frente na estratégia de defesa -> indo para ataque
-def engage_traction():
-    motor_W.run(SPEED_RUN)
-
-def check_boundaries():
+def checarBordas():
     # Continuously check if the boundary is reached, and, if so, blocks the other behaviours and moves away from it
     
-    global roam_enabled
-    if sensor_D.distance() <= ENEMY_DISTANCE:
-        roam_enabled = True
+    global andar
+    if sensor_enemy.distance() <= ENEMY_DISTANCE:
+        andar = True
     
-    print("base colour", ground_colour)
+    print("base colour", cor_chao)
     while True:
-        value_L = sensor_L.reflection()
-        print("Edge detected!", ground_colour, value_L)
-        
-        # value_R = sensor_R.reflection()
-        if abs(value_L - ground_colour) >= EDGE_THRESHOLD:
-            roam_enabled = False  # Stop the other behaviours
-            print("Edge detected!", ground_colour, value_L)
-
+        value_L = sensor_cor.reflection()
+        #print("Color detected!", cor_chao, value_L)
+        if value_L >= (cor_chao + EDGE_THRESHOLD):
+            andar = False  # Stop the other behaviours
+            print("Color detected!", cor_chao, value_L)
             ev3.light.on(Color.YELLOW)
-            #ev3.screen.load_image("images/cat_afraid.png")
-            
-            # Semd this bitch upwards when contacted
-            engage_traction()
-            
-            # Back away from the edge
-            motor_L.dc(SPEED_RUN)
-            motor_R.dc(SPEED_RUN)
-            wait(100)
 
-            # Rotate away from the edge, depending on which sensor got triggered
-            '''if abs(value_L-ground_colour) >= abs(value_R-ground_colour):
-                motor_L.dc(SPEED_RUN)
-            else:
-                motor_R.dc(SPEED_RUN)
-            '''
+            # Stop the motors
             motor_L.stop(Stop.BRAKE)
             motor_R.stop(Stop.BRAKE)
 
+            # Reverse
+            motor_L.run_time(speed=-SPEED_RUN, time=500)
+            motor_R.run_time(speed=-SPEED_RUN, time=500)
+            motor_tracao.run_time(speed=-SPEED_RUN, time=500)
+
+            # Turn right
+            motor_L.run_time(speed=SPEED_RUN, time=1000)
+            motor_R.run_time(speed=-SPEED_RUN, time=1000)
+
             # Resume the other behaviours
-            roam_enabled = True
+            andar = True
             print("Roaming again")
         wait(10)
 
-# Initialisation
-motor_L = Motor(Port.A, Direction.CLOCKWISE)  # Direita
-motor_R = Motor(Port.B, Direction.CLOCKWISE)  # Esquerda
-motor_W = Motor(Port.C, Direction.COUNTERCLOCKWISE)  # Tração
 
-sensor_L = ColorSensor(Port.S1) # ESQUERDA
-#sensor_R = ColorSensor(Port.S2) # DIREITA
-sensor_D = UltrasonicSensor(Port.S2) #INFRAVERMELHO # Esquerda
-# sensor_E = UltrasonicSensor(Port.S3) #INFRAVERMELHO # Direita
+# Initialisation
+motor_L = Motor(Port.B, Direction.CLOCKWISE)#Esquerda
+motor_R = Motor(Port.A, Direction.CLOCKWISE)#Direita
+motor_tracao = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+
+sensor_cor = ColorSensor(Port.S1) # ESQUERDA
+sensor_enemy = UltrasonicSensor(Port.S2) #Ultrasonico # Esquerda
+
 timer = StopWatch()
-ground_colour = 0
-turn_right = 0
-velRotate = SPEED_SEARCH*turn_right
-velRotateother = - (SPEED_SEARCH* int(not turn_right))
-roam_enabled = True  # Global flag to enable/disable the roaming behaviour
+cor_chao = 0
+andar = True  # Global flag to enable/disable the roaming behaviour
+
 random.seed(int(time()*1000))
 turn_right = random.randint(0, 1)
+
+'''velRotate = SPEED_SEARCH*turn_right
+velRotateother = -velRotate'''
+
 print("Initial turn right", turn_right)
-ground_colour = sensor_L.reflection()
+cor_chao = sensor_cor.reflection()
 ev3.speaker.set_volume(volume=100, which="_all_")
-# ev3.speaker.set_speech_options(language="en", voice="whisperf")
-# ev3.speaker.say("Ready to rumble!")
 ev3.speaker.set_speech_options(language="pt-pt", voice="whisperf")
 ev3.speaker.say("LE GO LO MA NI A CO")
-# Wait for a hand to be placed in front of the sensor, then removed, to start the initial countdown
-ev3.speaker.beep(frequency=1000, duration=100)
-while sensor_D.distance() > 50:
-    pass
-ev3.speaker.beep(frequency=1000, duration=100)
-while sensor_D.distance() < 50:
-    pass
-countdown()
 
+
+# Wait for the middle button to be pressed to start the initial countdown
+'''ev3.speaker.beep(frequency=1000, duration=100)
+ev3.buttons.wait_for_pressed(ev3.buttons.middle)
+ev3.speaker.beep(frequency=1000, duration=100)
+countdown()
+'''
 # Match begin: run towards the edge before turning around
-motor_W.run_angle(speed=500, rotation_angle=135, then=Stop.HOLD, wait=False)
-Thread(target=lower_wedge).start()
-Thread(target=check_boundaries).start()  # Begin edge detection
-motor_L.dc(-SPEED_RUN)
-motor_R.dc(-SPEED_RUN)
-while roam_enabled == False:
+'''motor_tracao.run_angle(speed=500, rotation_angle=135, then=Stop.HOLD, wait=False)'''
+
+
+'''Thread(target=checarBordas).start()  # Begin edge detection
+'''
+
+while andar == False:
     pass  # Wait for the edge detection to detect the edge before starting the main behaviour
 
 # Main behaviour
 while True:
-    if roam_enabled:
+    print("Velocidade", SPEED_RUN)
+    if sensor_cor.reflection() <= cor_chao:
+        andar = True
+    if andar:
         timer.reset()
-        motor_L.run(SPEED_RUN)  # Sentido horário
-        motor_R.run(SPEED_RUN)  # Sentido anti-horário
+        motor_L.dc(SPEED_RUN)
+        motor_R.dc(-SPEED_RUN)
+        motor_tracao.dc(SPEED_RUN)
         ev3.light.on(Color.GREEN)
-        
-        while timer.time() < random.randint(1, 3)*2000 and roam_enabled:
-            if sensor_D.distance() <= ENEMY_DISTANCE and sensor_E.distance() > ENEMY_DISTANCE:
-                print("ENEMY detectado!", sensor_D.distance())
-                # Comportamento para lidar com inimigo
-                ev3.light.on(Color.RED)
-            elif sensor_D.distance() > ENEMY_DISTANCE and sensor_E.distance() <= ENEMY_DISTANCE:
-                print("ENEMY detectado!", sensor_D.distance())
-                # Comportamento para lidar com inimigo
-                ev3.light.on(Color.RED)
-            elif sensor_D.distance() <= ENEMY_DISTANCE and sensor_E.distance() <= ENEMY_DISTANCE:
-                print("ENEMY detectado!", sensor_D.distance())
-                # Comportamento para lidar com inimigo
+        while timer.time() < random.randint(1, 3)*2000 and andar:
+            if sensor_enemy.distance() <= ENEMY_DISTANCE:
+                print("ENEMY!", sensor_enemy.distance())
+                # Enemy found: full speed ahead
+                motor_L.dc(SPEED_RUN) # sentido antihorario
+                motor_R.dc(SPEED_RUN)
+                motor_tracao.dc(SPEED_RUN)
                 ev3.light.on(Color.RED)
             wait(10)
-        
-        # Trocar a direção após o tempo determinado
-        turn_right = int(not turn_right)
+        turn_right = int(not turn_right)  # Now turn the other way
         wait(10)

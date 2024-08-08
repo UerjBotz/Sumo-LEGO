5eb9432b98d23b8c7eff2a0f3d24802a1990fe2a
Ian <ian@phygitall.rio>
2024-08-08 19:37:01 -0300

    documentação

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Sumo LEGO
-Sumo lego ne :.
+### Escrito por Argos A. Maia
+
+1. *Constantes*
+   - **EDGE_THRESHOLD**:
+   - **ENEMY_DISTANCE**: checa a distância do meu robô contra o robô adversário
+   - **SPEED_RUN**: velocidade de combate
+   - **SPEED_SEARCH**: velocidade de giro
+
+2. *checarBordas()*:
+   Verifica se o robô está próximo da borda da arena, se checar a borda branca, o robô para e vira a ré.
+   - Só funciona se estiver com o **sensorColor** ligado
+
+3. *Motores*
+  ~~~python
+  motor_L = Motor(Port.B, Direction.CLOCKWISE)#Esquerda
+  motor_R = Motor(Port.A, Direction.CLOCKWISE)#Direita
+  motor_tracao = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+  ~~~~
+ A roda de tração deve ser invertida para o funcionamento do código
+
+ 
--- SUMOLEGO/teste2.py
+++ SUMOLEGO/teste2.py
@@ -16,14 +16,14 @@ import random
 from time import time
 
 EDGE_THRESHOLD = 5
-ENEMY_DISTANCE =  400
+ENEMY_DISTANCE =  370
 SPEED_RUN = 100  # Power 
-SPEED_SEARCH = 720  # Deg/s
+SPEED_SEARCH = 900  # Deg/s
 
 ev3 = EV3Brick()
 
 def countdown():
-    # Wait 5 seconds, as per DA RULEZ
+    # Contagem de 5 segundos obrigatória antes de começar a luta
     for i in range(5):
         wait(900)
         ev3.speaker.beep(frequency=1000, duration=100)
@@ -37,7 +37,7 @@ def checarBordas():
     
     print("base colour", cor_chao)
     while True:
-        value_L = sensor_cor.reflection()
+        #value_L = sensor_cor.reflection()
         #print("Color detected!", cor_chao, value_L)
         if value_L >= (cor_chao + EDGE_THRESHOLD):
             andar = False  # Stop the other behaviours
@@ -60,6 +60,8 @@ def checarBordas():
             # Resume the other behaviours
             andar = True
             print("Roaming again")
+        # if statemenr if it detects white color on the floor then stop the robot, turn reverse and turn right (faster)
+
         wait(10)
 
 
@@ -68,7 +70,7 @@ motor_L = Motor(Port.B, Direction.CLOCKWISE)#Esquerda
 motor_R = Motor(Port.A, Direction.CLOCKWISE)#Direita
 motor_tracao = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 timer = StopWatch()
-sensor_cor = ColorSensor(Port.S1) # ESQUERDA
+# sensor_cor = ColorSensor(Port.S1) # ESQUERDA
 sensor_enemy = UltrasonicSensor(Port.S2) #Ultrasonico # Esquerda
 
 cor_chao = 0
@@ -77,37 +79,23 @@ andar = True  # Global flag to enable/disable the roaming behaviour
 random.seed(int(time()*1000))
 turn_right = random.randint(0, 1)
 
-'''velRotate = SPEED_SEARCH*turn_right
-velRotateother = -velRotate'''
+# InfraRed Sensor by judge's request
+# State 1: pronto
+# State 2: start
+# State 3: stop
+
 
 print("Initial turn right", turn_right)
-cor_chao = sensor_cor.reflection()
-ev3.speaker.set_volume(volume=100, which="_all_")
-ev3.speaker.set_speech_options(language="pt-pt", voice="whisperf")
-ev3.speaker.say("LE GO LO MA NI A CO")
-
-
-# Wait for the middle button to be pressed to start the initial countdown
-'''ev3.speaker.beep(frequency=1000, duration=100)
-ev3.buttons.wait_for_pressed(ev3.buttons.middle)
-ev3.speaker.beep(frequency=1000, duration=100)
-countdown()
-'''
-# Match begin: run towards the edge before turning around
-'''motor_tracao.run_angle(speed=500, rotation_angle=135, then=Stop.HOLD, wait=False)'''
-
-
-'''Thread(target=checarBordas).start()  # Begin edge detection
-'''
-
-while andar == False:
-    pass  # Wait for the edge detection to detect the edge before starting the main behaviour
+# cor_chao = sensor_cor.reflection()
+# ev3.speaker.set_volume(volume=100, which="_all_")
+# ev3.speaker.set_speech_options(language="pt-pt", voice="whisperf")
+# ev3.speaker.say("LE GO LO MA NI A CO")
 
 # Main behaviour
 while True:
     print("Velocidade", SPEED_RUN)
-    if sensor_cor.reflection() <= cor_chao:
-        andar = True
+    # if sensor_cor.reflection() <= cor_chao:
+    #     andar = True
     if andar:
         timer.reset()
         motor_L.dc(SPEED_RUN)
@@ -123,17 +111,19 @@ while True:
                 motor_tracao.dc(SPEED_RUN)
                 ev3.light.on(Color.RED)
             # if stopped dectecting the enemy, stop the motors
-            if sensor_enemy.distance() > ENEMY_DISTANCE:
+            if sensor_enemy.distance() > ENEMY_DISTANCE: #isso aqui tá errado (e é provavelmente inútil)
                 motor_L.stop(Stop.BRAKE)
                 motor_R.stop(Stop.BRAKE)
                 motor_tracao.stop(Stop.BRAKE)
                 ev3.light.off()
+
             # search for the enemy if it's not in front of the robot, increase the time of rotation
             if sensor_enemy.distance() > ENEMY_DISTANCE:
                 motor_L.dc(SPEED_SEARCH*turn_right)
                 motor_R.dc(-SPEED_SEARCH*turn_right)
-                #motor_tracao.dc(SPEED_RUN)
+                #motor_tracao.dc(SPEED_RUN) #colocar aqui pra ele soltar o motor
                 ev3.light.on(Color.ORANGE)
+
             wait(10)
         turn_right = int(not turn_right)  # Now turn the other way
         wait(10)
